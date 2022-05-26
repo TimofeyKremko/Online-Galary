9838a0e6d113f2598204be451999f1038aa30ad0
Timofey Kremko <timkremko@gmail.com>
2022-05-26 21:42:07 +0300

    fix bugs

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('.button__form');
 const emailInput = document.querySelector('.email__form');
 const textInput = document.querySelector('.text__form');
-const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z])$/;
+const reg = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
 button.addEventListener('click', (e) => {
 if(emailInput.value && textInput.value && reg.test(emailInput.value) == true) {
         Swal.fire(
